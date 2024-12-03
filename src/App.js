e7c8f457fb1b490decd5015f5ef8b85091ff0632
Antoun Atallah <70267873+itsantoun@users.noreply.github.com>
2024-12-03 14:31:05 +0200

    Draft3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/login'; // Import the Login component
 import Admin from './Admin/admin'; // Import the Admin component
 import BarcodeScanner from './Pages/BarcodeScanner'; // Import the BarcodeScanner component

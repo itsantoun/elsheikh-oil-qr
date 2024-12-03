cb1c4943e3eaeddc8190a2daf187eb47e909c045
Antoun Atallah <70267873+itsantoun@users.noreply.github.com>
2024-12-03 15:33:06 +0200

    Draft4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
+// import React from 'react';
+// import BarcodeScanner from './Pages/BarcodeScanner';
+
+// const App = () => {
+//   const handleScanSuccess = (decodedText, decodedResult) => {
+//     console.log(`Scanned code: ${decodedText}`);
+//     // You can implement additional logic or display the scanned product here
+//   };
+
+//   return (
+//     <div>
+//       <h1>Barcode Scanner</h1>
+//       <p>Scan a barcode to fetch product details from the database.</p>
+//       <BarcodeScanner onScanSuccess={handleScanSuccess} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BarcodeScanner from './Pages/BarcodeScanner';
 import Admin from './Admin/admin';
-import Login from './Pages/login';
+import Login from './Pages/login'
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState('login');
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'login':
-        return <Login setCurrentPage={setCurrentPage} />;
-      case 'scanner':
-        return <BarcodeScanner />;
-      case 'admin':
-        return <Admin />;
-      default:
-        return <Login setCurrentPage={setCurrentPage} />;
-    }
-  };
-
   return (
-    <div>
-      {renderPage()}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/scanner" element={<BarcodeScanner />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 };
 

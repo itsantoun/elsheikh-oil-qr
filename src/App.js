3ca3ea8993173f775037b58c84b92e014fbc3acc
Antoun Atallah <70267873+itsantoun@users.noreply.github.com>
2024-12-03 14:29:04 +0200

    Draft3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/login'; // Import the Login component
 import Admin from './Admin/admin'; // Import the Admin component
 import BarcodeScanner from './Pages/BarcodeScanner'; // Import the BarcodeScanner component
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/admin" element={<Admin />} />
-    //     <Route path="/scanner" element={<BarcodeScanner />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/scanner" element={<BarcodeScanner />} />
+      </Routes>
+    </Router>
   );
 };
 
